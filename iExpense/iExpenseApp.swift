9e16967dbe9bbad1ea12f31163090cade02d745d
SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>
2023-12-16 23:27:21 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Sanjay Siddharth on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
